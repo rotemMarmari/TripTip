9276de3096a999e5fe19b82d8dda928c332143fc
Rotem Marmari <156243829+rotemMarmari@users.noreply.github.com>
2025-01-29 00:01:47 +0200

    Update Map.jsx

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -9,8 +9,6 @@ const Map = ({ lat, lon, attractions }) => {
   const markersLayer = useRef(L.layerGroup());
 
   useEffect(() => {
-    useEffect(() => {
-    // Configure Leaflet marker icons
     L.Icon.Default.mergeOptions({
       iconRetinaUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
       iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
